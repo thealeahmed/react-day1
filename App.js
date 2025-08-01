114d9fa40f7a596c6d8088395c61d12513ac82e8
thealeahmad <aliahmad111486@gmail.com>
2025-08-01 21:59:02 +0500

    initial commit

--- App.js
+++ App.js
@@ -6,10 +6,12 @@
     // );
     // const root=ReactDOM.createRoot(document.getElementById('root'));
     // root.render(heading);
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
 const parent=React.createElement("div",{id:"parent"},
    [ React.createElement("div",{id:"child1"},[
         React.createElement("h1",{id:"h1"},"Hello World using react !",
-        React.createElement("h2",{id:"h2"},"This is a heading inside child1")), 
+        React.createElement("h2",{id:"h2"},"This is my first react app")), 
         React.createElement("h2",{id:"h2"},"This is a heading inside parent"),
         ]),
         React.createElement("div",{id:"child1"},[
---
+++ README.md
@@ -0,0 +1,19 @@
+# React Day 2🚀 
+
+# parcel
+-Dev build
+-local sever
+-HMR- Hot module Replacement
+-parcel usese a file watching algorithm-written in C++ 
+-Caching-Faster Builds
+-Image optimization 
+-Poduction-minify code
+-bundling
+-compressing the files 
+-consistent hashing (it keeps a track )
+-code splitting 
+-differential bundling-when your app can be opened inside internet explorer and every other browser it gives support older browsers
+-Diagnostic
+-Error Handling 
+-HTTPs Server 
+-tree shaking algorithm-remove unused code   
