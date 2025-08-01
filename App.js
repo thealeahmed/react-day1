2da8f36bc433f7b8209d34ce4d5b7925176adc85
thealeahmad <aliahmad111486@gmail.com>
2025-08-01 18:03:39 +0500

    React Day1

---
+++ App.js
@@ -0,0 +1,22 @@
+
+    // const heading=React.createElement(
+    //     'h1',
+    //     {id: 'h1' },
+    //     'Hello World using React!'
+    // );
+    // const root=ReactDOM.createRoot(document.getElementById('root'));
+    // root.render(heading);
+const parent=React.createElement("div",{id:"parent"},
+   [ React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{id:"h1"},"Hello World using react !",
+        React.createElement("h2",{id:"h2"},"This is a heading inside child1")), 
+        React.createElement("h2",{id:"h2"},"This is a heading inside parent"),
+        ]),
+        React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{id:"h1"},"Hello World using react !",
+        React.createElement("h2",{id:"h2"},"This is a heading inside child2")), 
+        React.createElement("h2",{id:"h2"},"This is a heading inside parent"),
+        ]),
+    ]);
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
